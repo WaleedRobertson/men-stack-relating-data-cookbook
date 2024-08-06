2c19ab6474bc150b80551c7fba2ad607c02155b1
Waleed Robertson <waleedrbrtsn1@email.com>
2024-08-06 17:00:45 -0400

    Working on lab

--- models/user.js
+++ models/user.js
@@ -9,8 +9,21 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: [foodSchema],
 });
 
+// user.js
+
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  }
+});
+
+
 const User = mongoose.model('User', userSchema);
+const Food = mongoose.model('Food', foodSchema);
 
 module.exports = User;
+module.exports = Food;
